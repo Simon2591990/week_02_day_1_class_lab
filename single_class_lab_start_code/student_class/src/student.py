f97dadb019e82a14d357784ee67ed81509a99a4d
Simon Charters <simon259@hotmail.co.uk>
2020-11-23 13:59:43 +0000

    finished student_class

--- single_class_lab_start_code/student_class/src/student.py
+++ single_class_lab_start_code/student_class/src/student.py
@@ -1,1 +1,18 @@
 
+class Student:
+
+    def __init__(self, name, cohort):
+        self.name = name
+        self.cohort = cohort
+
+
+
+    def talk(self):
+        return "I can talk!"
+
+    def say_favourite_language(self,fave_language):
+        return (f"I love {fave_language}")
+
+        
+
+
